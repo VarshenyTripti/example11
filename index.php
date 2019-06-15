619ca7ca7ecbe49574ef206eba8937f11ef53672
VarshenyTripti <51847343+VarshenyTripti@users.noreply.github.com>
2019-06-15 23:29:00 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,42 +1,48 @@
+<?php 
+	session_start(); 
+
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: login.php");
+	}
+
+?>
+<!DOCTYPE html>
 <html>
- <head>
-  <title>Sudoku</title>
-  <link href="css/sudoku-style.css" rel="stylesheet" type="text/css">
-  <link href="css/animate.css" rel="stylesheet" type="text/css">
-  <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Home Page</h2>
+	</div>
+	<div class="content">
 
-  <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-114x114-precomposed.png"/>
-  <link href="images/favicon.ico" rel="shortcut icon">
+		<!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
 
- </head>
-
- <body>
-  
-  <div id="header" class="header">sudoku</div>
-
-  <div id="welcome" class="welcome">
-    <h1>sudoku</h1>
-    <h2>by <a href="/" target="new">adam</a></h2>
-    <button onclick="Sudoku.game.init()">new game</button>
-  </div>
-
-  <div id="game-message" class="game-message"></div>
-
-  <div id="number-picker" class="number-picker"></div>
-  
-  <div id="solving-status" class="solving-status"></div>
-
-  <div id="board" class="board"></div>
-
-  <div id="buttons" class="buttons">
-   <button id="start-button" class="start" onclick="Sudoku.game.init()">start new game</button>
-   <button id="solve-button" class="solve" onclick="Sudoku.game.validateBeforeSolve()">solve board</button>
-  </div>
-
-  <script type="text/javascript" src="js/jquery.js"></script>
-  <script type="text/javascript" src="js/underscore.js"></script>
-  <script type="text/javascript" src="js/sudoku.js"></script>
-
- </body>
-
+		<!-- logged in user information -->
+		<?php  if (isset($_SESSION['username'])) : ?>
+			<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+			<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+		<?php endif ?>
+	</div>
+		
+</body>
 </html>
